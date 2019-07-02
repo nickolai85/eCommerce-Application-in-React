00d012ce7112c3f2a5e541a79c89ce82bb902fac
Nicolai <organ.nicolai@gmail.com>
2019-07-02 09:15:42 +0300

    styled purchase detail

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -13,6 +13,7 @@ class PurchaseDetail extends Component {
     render() {
         const { className, orderNumber, orderDate, user,total,creditCard } = this.props;
         const { name, shippingAddress } = user;
+        const nameAddress = `${name}       ${shippingAddress}`
         return (
             <div className={`${className} purchase-detail`}>
                 <PurchaseDetailLabel
@@ -24,17 +25,19 @@ class PurchaseDetail extends Component {
                 title='Order Date'
                 value={orderDate}/>
                 <PurchaseDetailLabel
-                className='purchase-detail__order-shipping'
+                className='purchase-detail__shipping-address'
                 title='Shipping Address'
                 value={`${name}\n${shippingAddress}`}/>
                 <PurchaseDetailLabel
                 className='purchase-detail__total'
                 title='Total'
-                value={total}/>
+                value={nameAddress}/>
                  <PurchaseDetailLabel
                 className='purchase-detail__order-shipping'
                 title='Credit Card'
                 value={creditCard}/>
+                <a className='purchase-detail__track-shipment'>Track Shipment</a>
+                <a className='purchase-detail__print-receipt'>Print Receipt</a>
             </div>
         )
     }
