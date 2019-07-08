f5103563b58cd9201c356104e36a9d96e253a6f8
Nicolai <organ.nicolai@gmail.com>
2019-07-08 08:45:41 +0300

    setup order review component and added page title component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware ,compose } from "redux";
 import { Router, Switch, Route } from 'react-router-dom';
 import reducers from "./reducers";
 import Shop from './components/shop/shop';
-
+import Review from './components/order/review';
 
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
@@ -29,6 +29,7 @@ function main() {
             <Route path='/signup' exact component={Signup}/>
             <Route path='/account' exact component={Account}/>
             <Route path='/shop' exact component={Shop}/>
+            <Route path='/order/review' exact component={Review}/>
           </Switch>
         </Layout>
         </Router>
