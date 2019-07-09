0a78a736b4edc180457b7fc2b4fd791a89025ff8
Nicolai <organ.nicolai@gmail.com>
2019-07-09 22:59:03 +0300

    finished shipping form component and sub components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -58,7 +58,7 @@ class ShippingForm extends Component {
                 name='back'
                 short={true}
                 component={FormButton}/>
-                <OrderSummary className='shipping-form__summary'/>
+                <OrderSummary className='shipping-form__order-summary'/>
             </form>
         )
     }
