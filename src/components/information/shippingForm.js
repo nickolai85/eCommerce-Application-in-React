ac68442b2c57adaffa327f2174a7371abe8fad51
Nicolai <organ.nicolai@gmail.com>
2019-07-09 22:52:12 +0300

    built order summary component, infotitle and underlined title components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -5,7 +5,7 @@ import { reduxForm, Field } from 'redux-form';
 import { FormInput, FormButton } from '../formFields';
 
 import history from '../../history';
-
+import OrderSummary from './orderSummary';
 class ShippingForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
@@ -58,7 +58,7 @@ class ShippingForm extends Component {
                 name='back'
                 short={true}
                 component={FormButton}/>
-
+                <OrderSummary className='shipping-form__summary'/>
             </form>
         )
     }
