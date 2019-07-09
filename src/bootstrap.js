e8083d646a4f06f0a60d41707a6c5749ee455a54
Nicolai <organ.nicolai@gmail.com>
2019-07-09 22:11:42 +0300

    built the information routes and container components payment and shipping js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,6 +6,8 @@ import { Router, Switch, Route } from 'react-router-dom';
 import reducers from "./reducers";
 import Shop from './components/shop/shop';
 import Review from './components/order/review';
+import Shipping from './components/information/shipping';
+import Payment from './components/information/payment';
 
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
@@ -30,6 +32,8 @@ function main() {
             <Route path='/account' exact component={Account}/>
             <Route path='/shop' exact component={Shop}/>
             <Route path='/order/review' exact component={Review}/>
+            <Route path='/information/shipping' exact component={Shipping}/>
+            <Route path='/information/payment' exact component={Payment}/>
           </Switch>
         </Layout>
         </Router>
